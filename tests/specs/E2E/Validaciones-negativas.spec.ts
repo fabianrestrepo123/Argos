b568d2429c00a17829b9f5029589db25eaf8763b
fabianrestrepo123 <fabianrestrepo123@gmail.com>
2026-01-31 16:48:23 -0500

    Se comenta  escenario alternativo

--- tests/specs/E2E/Validaciones-negativas.spec.ts
+++ tests/specs/E2E/Validaciones-negativas.spec.ts
@@ -3,17 +3,17 @@ import { InicioPage } from "../../../pages/Front/InicioPage";
 import { ProductosPage } from "../../../pages/Front/ProductosPage";
 import { CarritoPage } from "../../../pages/Front/CarritoPage";
 
-test("ECompra con formulario vacio", async ({ page }) => {
-  const inicio = new InicioPage(page);
-  const productos = new ProductosPage(page);
-  const carrito = new CarritoPage(page);
-
-  await inicio.abrirDemoBlaze();
-  await productos.irAlCarrito();
-  await carrito.abrirModalCompra();
-  await carrito.botonComprar();
-  await page.waitForTimeout(10000);
-
-
-  
-});
+//test("ECompra con formulario vacio", async ({ page }) => {
+//  const inicio = new InicioPage(page);
+//  const productos = new ProductosPage(page);
+//  const carrito = new CarritoPage(page);
+//
+//  await inicio.abrirDemoBlaze();
+//  await productos.irAlCarrito();
+//  await carrito.abrirModalCompra();
+//  await carrito.botonComprar();
+//  await page.waitForTimeout(10000);
+//
+//
+//  
+//});
